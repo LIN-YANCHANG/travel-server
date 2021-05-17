93797c6ad7a9ada93eeb8fcab228d7a903d1caf3
LIN-jack <apple65650505@gmail.com>
2021-05-17 17:40:44 +0800

    update

---
+++ routes/taipei.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
+const Routes = express.Router()
+
+// path.resolve 抓更目錄
+Routes.use(express.static(path.resolve('dist')))
+
+Routes.use('*', (req, res) => {
+    let html = fs.readFileSync(path.resolve('dist/index.html'), 'utf-8')
+    res.send(html)
+})
+
+
+module.exports = Routes
